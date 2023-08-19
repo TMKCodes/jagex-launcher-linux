02c8f77f1588e82f6ac6c69f71422b620b21536b
Toni Lukkaroinen <tmkcodes@gmx.com>
2023-08-19 09:11:35 +0300

    Change AppImage runelite to Flatpak runelite

--- resources/runelite.sh
+++ resources/runelite.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-"$(pwd)"/Games/RuneLite/RuneLite.AppImage --appimage-extract-and-run
+set -e
+flatpak run net.runelite.RuneLite || \
+flatpak-spawn --host flatpak run \
+    --env=JX_ACCESS_TOKEN="$JX_ACCESS_TOKEN" \
+    --env=JX_CHARACTER_ID="$JX_CHARACTER_ID" \
+    --env=JX_DISPLAY_NAME="$JX_DISPLAY_NAME" \
+    --env=JX_REFRESH_TOKEN="$JX_REFRESH_TOKEN" \
+    --env=JX_SESSION_ID="$JX_SESSION_ID" \
+net.runelite.RuneLite
