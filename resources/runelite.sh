78b4699c2dd66def743d6a727ac8e155ccf40461
Toni Lukkaroinen <tmkcodes@gmx.com>
2023-08-23 20:18:32 +0300

    fix runelite.sh

--- resources/runelite.sh
+++ resources/runelite.sh
@@ -10,9 +10,11 @@ echo "Updating net.runelite.RuneLite..."
 flatpak update net.runelite.RuneLite
 
 echo "Running net.runelite.RuneLite..."
-flatpak run net.runelite.RuneLite \
+flatpak run net.runelite.RuneLite || \
+flatpak-spawn --host flatpak run \
     --env=JX_ACCESS_TOKEN="$JX_ACCESS_TOKEN" \
     --env=JX_CHARACTER_ID="$JX_CHARACTER_ID" \
     --env=JX_DISPLAY_NAME="$JX_DISPLAY_NAME" \
     --env=JX_REFRESH_TOKEN="$JX_REFRESH_TOKEN" \
-    --env=JX_SESSION_ID="$JX_SESSION_ID"
+    --env=JX_SESSION_ID="$JX_SESSION_ID" \
+    net.runelite.RuneLite
